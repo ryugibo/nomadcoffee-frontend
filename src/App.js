5df8b26c9b98d4be7527a1000a60f34140202a4a
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-06-03 21:18:18 +0900

    apollo and authentication

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import { useReactiveVar } from "@apollo/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { isLoggedInVar } from "./apollo";
 import Home from "./screens/home";
 import Login from "./screens/login";
 import NotFound from "./screens/NotFound";
 
 function App() {
-  const isLoggedIn = false;
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
   return (
     <div>
       <Router>
