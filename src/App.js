574e78995fcd31573158a36eac4f98d2113220a7
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-06-03 21:02:32 +0900

    Routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./screens/home";
+import Login from "./screens/login";
+import NotFound from "./screens/NotFound";
+
 function App() {
-  return <div></div>;
+  const isLoggedIn = false;
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={isLoggedIn ? <Home /> : <Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
