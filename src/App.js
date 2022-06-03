90bf3ae705c4201cd73576d4deb18e6c232a8583
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-06-03 21:37:09 +0900

    styled components

--- src/App.js
+++ src/App.js
@@ -1,35 +1,24 @@
 import { useReactiveVar } from "@apollo/client";
-import userEvent from "@testing-library/user-event";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { darkModeVar, isLoggedInVar } from "./apollo";
 import Home from "./screens/home";
 import Login from "./screens/login";
 import NotFound from "./screens/NotFound";
-
-const lightTheme = {
-  fontColor: "#2c2c2c",
-  bgColor: "lightgray",
-};
-
-const darkTheme = {
-  fontColor: "lightgray",
-  bgColor: "#2c2c2c",
-};
+import { darkTheme, GlobalStyles, lightTheme } from "./styles";
 
 function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
   const darkMode = useReactiveVar(darkModeVar);
   return (
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-      <div>
-        <Router>
-          <Routes>
-            <Route exact path="/" element={isLoggedIn ? <Home /> : <Login />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Router>
-      </div>
+      <GlobalStyles />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={isLoggedIn ? <Home /> : <Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
     </ThemeProvider>
   );
 }
--- src/screens/login.js
+++ src/screens/login.js
@@ -1,9 +1,7 @@
 import { darkModeVar, isLoggedInVar } from "../apollo";
 import styled from "styled-components";
 
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`;
+const Container = styled.div``;
 
 const Title = styled.h1`
   color: ${(props) => props.theme.fontColor};
