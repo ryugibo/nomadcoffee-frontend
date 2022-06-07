16433bd25aa7bdbf402f6c0497e3a955d5f6c879
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-06-08 02:34:24 +0900

    Challenge 8 (#2)

--- src/apollo.js
+++ src/apollo.js
@@ -1,4 +1,24 @@
-import { makeVar } from "@apollo/client";
+import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
 
-export const isLoggedInVar = makeVar(false);
+const TOKEN = "TOKEN";
+const DARK_MODE = "DARK_MODE";
+
+export const isLoggedInVar = makeVar(Boolean(localStorage.getItem(TOKEN)));
+
+export const logUserIn = (token) => {
+  localStorage.setItem(TOKEN, token);
+  isLoggedInVar(true);
+};
+
+export const logUserOut = () => {
+  localStorage.removeItem(TOKEN);
+  isLoggedInVar(false);
+  window.location.reload();
+};
+
 export const darkModeVar = makeVar(false);
+
+export const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
---
+++ src/components/Container.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  form {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+export default Container;
